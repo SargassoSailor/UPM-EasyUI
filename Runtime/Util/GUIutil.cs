ec21a428206a329c9b96b1327a9794a0b659cf33
ChrisAshtear <chrisashtear@gmail.com>
2021-07-31 18:16:33 -0400

    Added line drawing code for editor

--- Runtime/Util/GUIutil.cs
+++ Runtime/Util/GUIutil.cs
@@ -58,7 +58,15 @@ public static class ObjectToDictionaryHelper
 
 public static class GUIutil
 {
-
+    public static void DrawUILine(Color color, int thickness = 2, int padding = 10)
+    {
+        Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
+        r.height = thickness;
+        r.y += padding / 2;
+        r.x -= 2;
+        r.width += 6;
+        EditorGUI.DrawRect(r, color);
+    }
 
     public static void matchObjSizeWithParent(GameObject child, GameObject parent)
     {
