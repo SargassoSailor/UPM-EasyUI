71980d11b8f51e88b07b1512fe848be48c9f54da
ChrisAshtear <chrisashtear@gmail.com>
2022-06-09 14:29:44 -0400

    simple pause now supports the new input system. added version defines so simple pause supports both input systems at the same time.

--- Runtime/Menu/SimplePause.cs
+++ Runtime/Menu/SimplePause.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 using EUI;
+using UnityEngine.InputSystem;
 
 public class SimplePause : MonoBehaviour
 {
@@ -11,6 +12,16 @@ public class SimplePause : MonoBehaviour
     public string pausePanelName = "PausePanel";
     public bool showMenuWhenPaused = true;
     bool canPause = false;
+#if NEW_INPUT
+    public InputAction pauseKeybinds;
+#endif
+    private void Start()
+    {
+#if NEW_INPUT
+        pauseKeybinds.Enable();
+        pauseKeybinds.performed += DoPause;
+#endif
+    }
 
     public void SetPauseAbility(bool canPause)
     {
@@ -20,7 +31,8 @@ public class SimplePause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+#if !NEW_INPUT
+        //InputSystem.
         //Check if the Cancel button in Input Manager is down this frame (default is Escape key) and that game is not paused, and that we're not in main menu
         if (Input.GetButtonDown("Cancel") && !isPaused && canPause) // fix this
         {
@@ -33,9 +45,17 @@ public class SimplePause : MonoBehaviour
             //Call the UnPause function to unpause the game
             UnPause();
         }
-
+#endif
     }
-
+#if NEW_INPUT
+    public void DoPause(InputAction.CallbackContext inp)
+    {
+        if(canPause)
+        {
+            DoPause(!isPaused);
+        }
+    }
+#endif
     public void DoPause(bool pause = true)
     {
         MenuPause(pause);
