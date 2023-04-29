6a8606dd5863db4c976539c0d7d9704abef7ee70
ChrisAshtear <chrisashtear@gmail.com>
2023-04-29 01:21:39 -0400

    resolve missing expirmental pack

--- Runtime/Menu/Components/populateItem.cs
+++ Runtime/Menu/Components/populateItem.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Experimental.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor.SceneManagement;
 #endif
