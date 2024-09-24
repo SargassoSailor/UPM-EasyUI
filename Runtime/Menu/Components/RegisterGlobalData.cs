1096816595cfe564c1ceaec695eb7e840a50d3d4
Ryan Wine <chrisashtear@gmail.com>
2024-09-23 23:57:53 -0400

    renaming some functions

--- Runtime/Menu/Components/RegisterGlobalData.cs
+++ Runtime/Menu/Components/RegisterGlobalData.cs
@@ -9,52 +9,52 @@ using UnityEngine;
 public delegate List<string> PopulateOptionsAction();
 public static class DataRegister
 {
-    public static Dictionary<string, PopulateOptionsAction> optionsList = new Dictionary<string, PopulateOptionsAction>();
-    public static Dictionary<string, string> optionsListByCategory = new Dictionary<string, string>();
+    public static Dictionary<string, PopulateOptionsAction> itemList = new Dictionary<string, PopulateOptionsAction>();
+    public static Dictionary<string, string> itemListByCategory = new Dictionary<string, string>();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     public static void Init()
     {
-        if(optionsList == null)
+        if(itemList == null)
         {
-            optionsList = new Dictionary<string, PopulateOptionsAction>();
+            itemList = new Dictionary<string, PopulateOptionsAction>();
         }
         
 
         Debug.Log("Init Register");
     }
 
-    public static void AddOption(string name, PopulateOptionsAction option)
+    public static void AddData(string name, PopulateOptionsAction option)
     {
-        bool result = optionsList.TryAdd(name.ToLower(), option);
+        bool result = itemList.TryAdd(name.ToLower(), option);
     }
 
-    public static PopulateOptionsAction GetOption(string name)
+    public static PopulateOptionsAction GetData(string name)
     {
-        bool result = optionsList.TryGetValue(name.ToLower(), out var option);
+        bool result = itemList.TryGetValue(name.ToLower(), out var option);
         return result ? option : null;
     }
 
     public static List<string> GetFullOptions(string name)
     {
-        bool result = optionsList.TryGetValue(name.ToLower(), out PopulateOptionsAction optionCall);
+        bool result = itemList.TryGetValue(name.ToLower(), out PopulateOptionsAction optionCall);
         List<string> fullOpts = optionCall.Invoke();
 
         return fullOpts;
     }
 
-    public static void AddOptionForCategory(string category, string optionName)
+    public static void AddDataForCategory(string category, string optionName)
     {
-        bool result = optionsListByCategory.TryAdd(category.ToLower(), optionName.ToLower());
+        bool result = itemListByCategory.TryAdd(category.ToLower(), optionName.ToLower());
 
     }
 
-    public static PopulateOptionsAction GetOptionForCategory(string category)
+    public static PopulateOptionsAction GetDataForCategory(string category)
     {
-        bool result = optionsListByCategory.TryGetValue(category.ToLower(), out string option);
+        bool result = itemListByCategory.TryGetValue(category.ToLower(), out string option);
         if (result)
         {
-            PopulateOptionsAction optionCall = optionsList[option];
+            PopulateOptionsAction optionCall = itemList[option];
             return optionCall;
         }
         return null;
@@ -62,7 +62,7 @@ public static class DataRegister
 
     public static List<string> GetAllOptionNames()
     {
-        return optionsList.Keys.ToList();
+        return itemList.Keys.ToList();
     }
 }
 
